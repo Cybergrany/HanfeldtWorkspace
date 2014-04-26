fb18daaec7b90204090748f3b09752c6960196bb
Cybergrany <cybergrany404@gmail.com>
2014-04-26 13:11:09 +0100

    Player movement
    LOOK IT MOVES! A and D to move left/right

--- Hanfeldt Game/src/com/hanfeldt/game/Player.java
+++ Hanfeldt Game/src/com/hanfeldt/game/Player.java
@@ -5,15 +5,22 @@ public class Player extends Entity {
 	//TODO: Smooth out movement
 	public static int movement = 0;
 	
-	public static int x, y;
+	public static int x, y = 95;
 	public static int dir = 0;
+	
+	static int relativeX = 0;
 	
 	public Player(){
 		
 	}
 	
 	public static void tick(){
-		
+		if(x > Main.sizeX - 90){
+			x--;
+			relativeX++;
+		}else{
+			x = movement - relativeX;
+		}
 	}
 	
 	public void draw(){
