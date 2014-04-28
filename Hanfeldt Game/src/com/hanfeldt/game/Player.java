a15d4b3bd3eebcb63d57ca8fa7d6b2d0528047ed
Ronan Hanley <bormear@gmail.com>
2014-04-28 18:38:59 +0100

    Player changes direction
    He changes direction now

--- Hanfeldt Game/src/com/hanfeldt/game/Player.java
+++ Hanfeldt Game/src/com/hanfeldt/game/Player.java
@@ -12,16 +12,16 @@ public class Player extends Entity {
 	public void tick(){
 		if(Main.aDown) {
 			changeX(-speed);
+			direction = false;
 		}
 		
 		if(Main.dDown) {
 			changeX(speed);
+			direction = true;
 		}
-		
-		System.out.println(getX());
 	}
 	
 	public void draw(Graphics g) {
-		sprite.draw(g, (Main.sizeX /2) - (Main.tileSize /2), getY());
+		sprite.draw(g, (Main.sizeX /2) - (Main.tileSize /2), getY(), direction);
 	}
 }
--- Hanfeldt Game/src/com/hanfeldt/game/Sprite.java
+++ Hanfeldt Game/src/com/hanfeldt/game/Sprite.java
@@ -5,13 +5,24 @@ import java.awt.image.BufferedImage;
 
 public class Sprite {
 	private BufferedImage image;
+	private int width, height;
 	
 	public Sprite(SpriteSheet sheet, int x, int y, int w, int h) {
 		image = sheet.getImage(x, y, w, h);
+		width = w;
+		height = h;
 	}
 	
 	public void draw(Graphics g, int x, int y) {
 		g.drawImage(image, x, y, null);
+	}
+	
+	public void draw(Graphics g, int x, int y, boolean direction) {
+		if(direction) {
+			g.drawImage(image, x, y, null);
+		}else{
+			g.drawImage(image, x + (Main.tileSize), y, - Main.tileSize * width, Main.tileSize *height, null);
+		}
 	}
 	
 }
