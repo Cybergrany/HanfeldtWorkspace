5ceb4f86090519d076c124bddc38fd31fee022b4
Cybergrany <cybergrany404@gmail.com>
2014-05-06 22:24:37 +0100

    Commit So I can sync.

--- Hanfeldt Game/src/com/hanfeldt/game/event/NpcEvents.java
+++ Hanfeldt Game/src/com/hanfeldt/game/event/NpcEvents.java
@@ -1,10 +1,14 @@
 package com.hanfeldt.game.event;
 
 import com.hanfeldt.game.entity.npc.Npc;
+import com.hanfeldt.game.entity.npc.Zombie;
 
 public class NpcEvents {
 	
+	public static int zombie_Id = 1;
+	
 	private Npc npc;
+	private Zombie zombie;
 	
 	public NpcEvents(Npc npc){
 		this.npc = npc;
@@ -14,6 +18,9 @@ public class NpcEvents {
 		
 	}
 	
-	
-
+	public void damageNpc(int damage, int id){
+		if(id == zombie_Id){
+			zombie.changeHealth(-damage);
+		}
+	}
 }
