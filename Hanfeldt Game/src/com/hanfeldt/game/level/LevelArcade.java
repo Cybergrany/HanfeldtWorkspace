a7c412d634dffead9126adbf19a282218ad81736
Cybergrany <cybergrany404@gmail.com>
2014-07-05 00:28:08 +0100

    Unfinished random level gen lul
    Kinda works. It only does it for a few blocks so far, gonna work  on
    generating as you walk. I'd have got more done, but got sidetracked by
    setting up a stream...

--- Hanfeldt Game/src/com/hanfeldt/game/level/LevelArcade.java
+++ Hanfeldt Game/src/com/hanfeldt/game/level/LevelArcade.java
@@ -1,12 +1,14 @@
 package com.hanfeldt.game.level;
 
 import java.awt.Graphics;
+import java.util.Random;
 
 import com.hanfeldt.game.Camera;
 import com.hanfeldt.game.Main;
 import com.hanfeldt.game.entity.Player;
 import com.hanfeldt.game.entity.npc.Spawner;
 import com.hanfeldt.game.tile.Air;
+import com.hanfeldt.game.tile.CementCore;
 import com.hanfeldt.game.tile.CementFloor;
 import com.hanfeldt.game.tile.Tile;
 
@@ -17,7 +19,7 @@ public class LevelArcade extends Level {
 	
 	public LevelArcade(Player p) {
 		sizeY = Main.HEIGHT / Main.tileSize;
-		sizeX = Main.WIDTH / Main.tileSize;
+		sizeX = Main.WIDTH / Main.tileSize * 2;
 		tiles = new Tile[sizeX][sizeY];
 		spawner = new Spawner();
 		
@@ -27,20 +29,64 @@ public class LevelArcade extends Level {
 			}
 		}
 		
-		generateLevel();
+		generateLevel(p);
 		
 		setBg(level);
 		
 		player = p;
 	}
 	
-	public void generateLevel(){
+	public void generateLevel(Player p){
+		
+		//Rough Terrain
 		for(int y=0; y<sizeY; y++) {
 			for(int x=0; x<sizeX; x++) {
 				if(y > sizeY - 2){
+					
+					//Floor (With gaps)
+					if(new Random().nextInt(100) > 10){
+						try{
+							tiles[x][y] = new CementCore(x, y);
+						}catch(Exception e){}
+					}
+					
+					
+//					
+//					if(new Random().nextInt(100) > 90){
+//						try{
+//							int r = new Random().nextInt(Main.HEIGHT - 3);
+//							for(int i = 0; i <= r; i++){
+//								tiles[x - i][y - i] = new CementFloor(x - i, y - i);
+//							}
+//						}catch(Exception e){}
+//					}
+				}
+				
+//				Alright, I'm gonna start on some random level generation
+//				I'm doing pretty shit
+//				yolo
+//				One min, gotta add the game itself to obs
+				if (new Random().nextInt(100) > 80){
 					try{
-						tiles[x][y] = new CementFloor(x, y);
+						if(tiles[x][y] instanceof CementCore)
+						tiles[x][y - 1] = new CementCore(x, y - 1);
 					}catch(Exception e){}
+				}
+			}
+		}
+		
+		//Corrections
+		for(int y=0; y<sizeY; y++) {
+			for(int x=0; x<sizeX; x++) {
+				//Smooth out floor
+				try{
+					if(tiles[x][y] instanceof CementCore && tiles[x][y - 1] instanceof Air){
+							tiles[x][y] = new CementFloor(x, y);
+					}
+				}catch(Exception e){}
+				//Left-hand side wall
+				if(x < 1){
+					tiles[x][y] = new CementCore(x, y);
 				}
 			}
 		}
