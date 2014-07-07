a68486d71c0643eb5708ac0b416dc91d339e51b9
Ronan Hanley <bormear@gmail.com>
2014-07-07 20:43:28 +0100

    Hiscore client working
    Try it yourself! Just pull this and play through story (use debugcheats
    if yer lazy). Dun forget to set yer username!

--- Hanfeldt Game/src/com/hanfeldt/game/server/Server.java
+++ Hanfeldt Game/src/com/hanfeldt/game/server/Server.java
@@ -4,68 +4,39 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Server implements Runnable{
 	private static final short port = 21001;
 	private ServerSocket serverSock;
-	private Socket client;
+	private ArrayList<Socket> clients;
 	
 	public Server() {
 		try {
-			if(serverSock != null)
 			serverSock = new ServerSocket(port);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		clients = new ArrayList<Socket>();
 	}
 	
 	public void run() {
 		while(true) {
 			try {
-				if(serverSock!=null)
-				client = serverSock.accept();
+				System.out.println("Waiting for a client...");
+				clients.add(serverSock.accept());
+				System.out.println("Client connected. Starting send and receive thread...");
+				Thread serverThread = new Thread(new ServerThread(clients.get(clients.size() -1)));
+				serverThread.start();
+				System.out.println("Thread started.");
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-			new Thread(new Runnable() {
-				public void run() {
-					try {
-						PrintWriter writer = new PrintWriter(client.getOutputStream());
-						BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-						int hiscore = 0;
-						String hsName = null;
-						
-						BufferedReader hsReader = new BufferedReader(new FileReader("/hiscores.txt"));
-						String line = null;
-						while((line = hsReader.readLine()) != null) {
-							StringTokenizer st = new StringTokenizer(line, ";");
-							String name = st.nextToken();
-							int score = Integer.parseInt(st.nextToken());
-							st.nextToken();
-							if(score > hiscore) {
-								hiscore = score;
-								hsName = name;
-							}
-						}
-						hsReader.close();
-						writer.println(hsName + ":" + Integer.toString(hiscore));
-						writer.close();
-						String packetInput = reader.readLine();
-						PrintWriter scoreWriter = new PrintWriter(new BufferedWriter(new FileWriter("/hiscores.txt")), true);
-						scoreWriter.println(packetInput);
-						scoreWriter.close();
-						reader.close();
-					}catch(Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
 		}
 	}
 	
@@ -74,5 +45,46 @@ public class Server implements Runnable{
 		new Thread(server).start();
 	}
 	
+}
+
+class ServerThread implements Runnable {
+	private Socket client;
+	
+	public ServerThread(Socket client) {
+		this.client = client;
+	}
+	
+	public void run() {
+		try {
+			PrintWriter writer = new PrintWriter(client.getOutputStream());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			int hiscore = 0;
+			String hsName = null;
+			
+			BufferedReader hsReader = new BufferedReader(new FileReader("dev_resource/hiscores.txt"));
+			String line = null;
+			while((line = hsReader.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line, ";");
+				String name = st.nextToken();
+				int score = Integer.parseInt(st.nextToken());
+				st.nextToken();
+				if(score > hiscore) {
+					hiscore = score;
+					hsName = name;
+				}
+			}
+			hsReader.close();
+			writer.println(hsName + ";" + Integer.toString(hiscore));
+			writer.flush();
+			String packetInput = reader.readLine();
+			PrintWriter scoreWriter = new PrintWriter(new BufferedWriter(new FileWriter("dev_resource/hiscores.txt", true)));
+			scoreWriter.println(packetInput + ";" + client.getInetAddress());
+			scoreWriter.close();
+			writer.close();
+			client.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
--- Hanfeldt Game/src/com/hanfeldt/game/state/GameWon.java
+++ Hanfeldt Game/src/com/hanfeldt/game/state/GameWon.java
@@ -2,8 +2,14 @@ package com.hanfeldt.game.state;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 import com.hanfeldt.game.Main;
 import com.hanfeldt.game.entity.Firework;
@@ -12,12 +18,37 @@ public class GameWon extends State {
 	private ArrayList<Firework> fireworks;
 	private Random rand;
 	private long nextFireworkTick = 0;
+	private boolean hsServerAvailable = false;
+	private static final String ip = "188.141.30.230";
+	private static final short port = 21001;
+	private String hsName = null;
+	private long bestHs = 0;
 	
 	public GameWon(Main main) {
 		super(main);
 		fireworks = new ArrayList<Firework>();
 		rand = new Random();
 		nextFireworkTick = Main.getGame().getTotalTicks() + rand.nextInt(30) +30;
+		String username = main.username;
+		long score = main.getPlayer().getScore();
+		try {
+			Socket server = new Socket();
+			server.connect(new InetSocketAddress(ip, port), 1000);
+			hsServerAvailable = true;
+			BufferedReader hsReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			PrintWriter sWriter = new PrintWriter(server.getOutputStream());
+			
+			StringTokenizer st = new StringTokenizer(hsReader.readLine(), ";");
+			hsName = st.nextToken();
+			bestHs = Long.parseLong(st.nextToken());
+			
+			sWriter.println(username + ";" + score);
+			sWriter.flush();
+			
+			server.close();
+		}catch(Exception e) {
+			hsServerAvailable = false;
+		}
 	}
 	
 	public void tick() {
@@ -49,6 +80,13 @@ public class GameWon extends State {
 		g.drawString("You finished with a score of:", Main.WIDTH /2 - (("You finished with a score of:".length() /2) *5), Main.HEIGHT /2 +30);
 		String scoreString = Long.toString(main.getPlayer().getScore());
 		g.drawString(scoreString, Main.WIDTH /2 - (scoreString.length() /2) *5, Main.HEIGHT /2 +40);
+		if(!hsServerAvailable) {
+			String string = "Hiscore server unavailable/waiting to transfer scores";
+			g.drawString(string, Main.WIDTH /2 - (string.length() /2) *5, Main.HEIGHT /2 +50);
+		}else{
+			String string = "Best score: " + hsName + " with a score of " + bestHs;
+			g.drawString(string, Main.WIDTH /2 - (string.length() /2) *5, Main.HEIGHT /2 +50);
+		}
 		for(int i=0; i<fireworks.size(); i++) {
 			fireworks.get(i).render(g);
 		}
