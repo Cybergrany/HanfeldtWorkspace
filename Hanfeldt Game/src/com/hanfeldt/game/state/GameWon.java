94b0e48af9340ed22786ce5cef43b5084bfcdac8
Cybergrany <cybergrany404@gmail.com>
2014-07-07 21:47:45 +0100

    #JustGitThings
    It messed up again

--- Hanfeldt Game/src/com/hanfeldt/game/state/GameWon.java
+++ Hanfeldt Game/src/com/hanfeldt/game/state/GameWon.java
@@ -2,53 +2,25 @@ package com.hanfeldt.game.state;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.StringTokenizer;
 
 import com.hanfeldt.game.Main;
 import com.hanfeldt.game.entity.Firework;
+import com.hanfeldt.game.server.Client;
 
 public class GameWon extends State {
-	private ArrayList<Firework> fireworks;
+	public static ArrayList<Firework> fireworks;
 	private Random rand;
 	private long nextFireworkTick = 0;
-	private boolean hsServerAvailable = false;
-	private static final String ip = "188.141.30.230";
-	private static final short port = 21001;
-	private String hsName = null;
-	private long bestHs = 0;
 	
 	public GameWon(Main main) {
 		super(main);
 		fireworks = new ArrayList<Firework>();
 		rand = new Random();
 		nextFireworkTick = Main.getGame().getTotalTicks() + rand.nextInt(30) +30;
-		String username = Main.username;
-		long score = main.getPlayer().getScore();
-		try {
-			Socket server = new Socket();
-			server.connect(new InetSocketAddress(ip, port), 1000);
-			hsServerAvailable = true;
-			BufferedReader hsReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-			PrintWriter sWriter = new PrintWriter(server.getOutputStream());
-			
-			StringTokenizer st = new StringTokenizer(hsReader.readLine(), ";");
-			hsName = st.nextToken();
-			bestHs = Long.parseLong(st.nextToken());
-			
-			sWriter.println(username + ";" + score);
-			sWriter.flush();
-			
-			server.close();
-		}catch(Exception e) {
-			hsServerAvailable = false;
-		}
+		
+		Client.sendScores();
 	}
 	
 	public void tick() {
@@ -80,23 +52,15 @@ public class GameWon extends State {
 		g.drawString("You finished with a score of:", Main.WIDTH /2 - (("You finished with a score of:".length() /2) *5), Main.HEIGHT /2 +30);
 		String scoreString = Long.toString(main.getPlayer().getScore());
 		g.drawString(scoreString, Main.WIDTH /2 - (scoreString.length() /2) *5, Main.HEIGHT /2 +40);
-		if(!hsServerAvailable) {
+		if(Client.hsServerAvailable) {
 			String string = "Hiscore server unavailable/waiting to transfer scores";
 			g.drawString(string, Main.WIDTH /2 - (string.length() /2) *5, Main.HEIGHT /2 +50);
 		}else{
-			String string = "Best score: " + hsName + " with a score of " + bestHs;
+			String string = "Best score: " + Client.hsName + " with a score of " + Client.bestHs;
 			g.drawString(string, Main.WIDTH /2 - (string.length() /2) *5, Main.HEIGHT /2 +50);
 		}
 		for(int i=0; i<fireworks.size(); i++) {
 			fireworks.get(i).render(g);
-		}
-	}
-	
-	public void removeFirework(Firework f) {
-		for(int i=0; i<fireworks.size(); i++) {
-			if(fireworks.get(i).equals(f)) {
-				fireworks.remove(i);
-			}
 		}
 	}
 	
